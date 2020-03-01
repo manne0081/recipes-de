6396414eab59d2918157bd8a8154898dfca5909a
Manuel <manuel@sony-vaio.fritz.box>
2020-03-01 20:51:06 +0100

    commit

--- src/app/recipe/recipe-start/recipe-start.component.ts
+++ src/app/recipe/recipe-start/recipe-start.component.ts
@@ -50,6 +50,42 @@ export class RecipeStartComponent {
         return n + m;
     }
 
+    // TEST...
+    val: string;
+    funTest (s: string, n: number) {
+        this.val = s + " " + this.fun2(n);
+        console.log(this.hello('function "hello": '));
+        console.log(this.helloA('function "helloA": ', 'hello world!'));
+        console.log(this.helloB('function "helloB": ', 'hello world!'));
+        console.log(this.helloC('function "helloC": ', 'hello world!'));
+        console.log(this.helloD());
+
+
+    }
+    fun2 (n: number): number {
+        return n*n;
+    }
+    hello = (x: string) => {
+        return x + "hello world!";
+    }
+    helloA (x: string, y: string) {
+        return x + y;
+    }
+    helloB = (x: string, y: string) => {
+        return x + y;
+    }
+    helloC = (x: string, y: string) => x + y;
+    helloD() {
+        return (x, y) => x + y;
+    }
+
+    hello1 = () => {
+        return "Hello World!";
+    }
+    hello2 = () => { "Hello World!" } 
+    hello3 = () => "Hello World!"
+    hello4 = (val2) => "Hello " + val2; 
+
 
     onGetData() {
         this.httpService.getData()
